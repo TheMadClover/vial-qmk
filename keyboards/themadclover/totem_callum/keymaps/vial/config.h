5b55e42f197cb626c7d32cfb23a98541a6cbd649
TheMadClover <130436262+TheMadClover@users.noreply.github.com>
2024-11-04 21:01:48 -0700

    merge

---
+++ keyboards/themadclover/totem_callum/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2D, 0x68, 0x1F, 0xDD, 0xFB, 0x0C, 0x52, 0x5B}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
---
+++ keyboards/themadclover/totem_callum/keymaps/vial/rules.mk
@@ -0,0 +1,9 @@
+SERIAL_DRIVER = vendor
+
+SRC += oneshot.c
+SRC += swapper.c
+
+COMBO_ENABLE = yes
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
