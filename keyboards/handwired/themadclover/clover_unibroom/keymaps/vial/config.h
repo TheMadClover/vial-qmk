79cb3fec044f7c549e832f45596879976de2adba
TheMadClover <130436262+TheMadClover@users.noreply.github.com>
2025-11-24 16:35:20 -0700

    Port Clover_UniBroom_Initial

---
+++ keyboards/handwired/themadclover/clover_unibroom/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x77, 0xB7, 0x1B, 0x98, 0x48, 0x50, 0xAE, 0x35}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
