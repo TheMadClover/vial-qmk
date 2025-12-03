0fa05e0f528782ab30d1583a3d5a6cc1131bc051
TheMadClover <130436262+TheMadClover@users.noreply.github.com>
2025-12-02 23:49:30 -0700

    keymap_update

--- keyboards/handwired/themadclover/clover_unibroom/keymaps/vial/keymap.c
+++ keyboards/handwired/themadclover/clover_unibroom/keymaps/vial/keymap.c
@@ -24,6 +24,7 @@ enum layers {
     SYM,
     NAV,
     NUM,
+    NUMP,
 };
 
 enum keycodes {
@@ -64,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [NAV] = LAYOUT_unibroom(
-        KC_TAB,  SW_WIN,  TAB_L,   TAB_R,   KC_VOLU, QK_RBT,  HOME,    KC_UP,   END,     KC_DEL,
+        KC_TAB,  SW_WIN,  TAB_L,   TAB_R,   KC_VOLU, KC_PSCR, HOME,    KC_UP,   END,     KC_DEL,
         OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_VOLD, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
-        SEL_L,   SEL_R,   BACK,    FWD,     KC_MPLY, XXXXXXX, KC_PGDN, KC_PGUP, EPIC, KC_ENT,
+        SEL_L,   SEL_R,   BACK,    FWD,     KC_MPLY, TG(4),   KC_PGDN, KC_PGUP, EPIC, KC_ENT,
                                    _______, _______, _______, _______
     ),
 
@@ -75,6 +76,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,  KC_F10,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
         KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,   KC_F8,   KC_F12,  KC_F2,   KC_F4,   KC_F6,
                                    _______, _______, _______, _______
+    ),
+
+    [NUMP] = LAYOUT_unibroom(
+        KC_TAB,  HOME,    KC_UP,   END,     KC_NUM,  KC_PMNS, KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, KC_PPLS, KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(4),   XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+                                   KC_BSPC, _______, KC_PDOT, KC_P0
     ),
 };
 
