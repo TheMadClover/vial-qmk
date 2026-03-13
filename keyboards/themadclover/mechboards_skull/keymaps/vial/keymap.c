14e4127f1d66ab5c50e51b4fe86f95a40366c76d
TheMadClover <130436262+TheMadClover@users.noreply.github.com>
2026-03-12 17:44:26 -0700

    add callum osm to kb-skull

--- keyboards/themadclover/mechboards_skull/keymaps/vial/keymap.c
+++ keyboards/themadclover/mechboards_skull/keymaps/vial/keymap.c
@@ -2,6 +2,29 @@
 
 #include "transactions.h"
 #include "color.h"
+#include "oneshot.h"
+#include "swapper.h"
+
+#define HOME KC_HOME
+#define END KC_END
+#define FWD A(KC_RGHT)
+#define BACK A(KC_LEFT)
+#define TAB_L C(S(KC_TAB))
+#define TAB_R C(KC_TAB)
+#define SEL_L C(S(KC_LEFT))
+#define SEL_R C(S(KC_RGHT))
+#define LA_SYM MO(SYM)
+#define LA_NAV MO(NAV)
+#define LA_DEF DF(DEF)
+#define EPIC C(KC_SPC)
+
+enum layers {
+    DEF,
+    SYM,
+    NAV,
+    NUM,
+    LED,
+};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,13 +41,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤Tab│   │Spc├───┘
       *                   └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x5_2(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                            MO(1), KC_BSPC,           KC_SPC,  KC_ENT
+    [DEF] = LAYOUT_split_3x5_2(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_QUOT,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_P,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN,
+                                   LA_NAV,  KC_SPC,  KC_LSFT, LA_SYM
     ),
-    [1] = LAYOUT_split_3x5_2(
+
+    [SYM] = LAYOUT_split_3x5_2(
+        KC_ESC,  KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD, KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
+        KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,  KC_HASH, OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        KC_PLUS, KC_PIPE, KC_AT,   KC_SLSH, KC_PERC, XXXXXXX, KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
+                                   _______, _______, _______, _______
+    ),
+
+    [NAV] = LAYOUT_split_3x5_2(
+        KC_TAB,  SW_WIN,  TAB_L,   TAB_R,   KC_VOLU, KC_PSCR, HOME,    KC_UP,   END,     KC_DEL,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_VOLD, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
+        SEL_L,   SEL_R,   BACK,    FWD,     KC_MPLY, TG(4),   KC_PGDN, KC_PGUP, EPIC, KC_ENT,
+                                   _______, _______, _______, _______
+    ),
+
+    [NUM] = LAYOUT_split_3x5_2(
+        KC_7,    KC_5,    KC_3,    KC_1,    KC_9,    KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
+        OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,  KC_F11,  KC_F10,  OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,
+        KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9,   KC_F8,   KC_F12,  KC_F2,   KC_F4,   KC_F6,
+                                   _______, _______, _______, _______
+    ),
+    [LED] = LAYOUT_split_3x5_2(
         QK_BOOT,    _______,    _______,    EY_TOGG,    UG_TOGG,                               _______,    _______,    _______,    _______,    _______,
         _______,    _______,    _______,    EY_HUE,     UG_HUEU,                               _______,    _______,    _______,    _______,    _______,
         _______,    _______,    EY_SAT ,    EY_VAL,     UG_NEXT,                               _______,    _______,    _______, _______,  _______,
@@ -66,7 +110,66 @@ void set_eyehsv(bool enable, uint8_t hue, uint8_t sat, uint8_t val){
     rgblight_driver.flush();
 }
 
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case LA_SYM:
+    case LA_NAV:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case LA_SYM:
+    case LA_NAV:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_CMD:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool sw_win_active = false;
+/*bool sw_lang_active = false;*/
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_swapper(
+        &sw_win_active, KC_LALT, KC_TAB, SW_WIN,
+        keycode, record
+    );
+    /*
+    update_swapper(
+        &sw_lang_active, KC_LCTL, KC_SPC, SW_LANG,
+        keycode, record
+    );
+    */
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_cmd_state, KC_LCMD, OS_CMD,
+        keycode, record
+    );
+
     switch (keycode) {
     case EY_TOGG:
         if (record->event.pressed) {
@@ -93,12 +196,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         break;
     }
+
     return true;
 };
 
 void user_sync_eyehsv_handler(uint8_t in_buflen, const void* in_data, uint8_t out_buflen, void* out_data) {
     const eyergb_config_t *m2s = (const eyergb_config_t*)in_data;
-    
+
     if (user_eyeconfig.raw != m2s->raw){
         user_eyeconfig.raw = m2s->raw;
         set_eyehsv(user_eyeconfig.enable,user_eyeconfig.hue,user_eyeconfig.sat,user_eyeconfig.val);
@@ -134,7 +238,7 @@ void housekeeping_task_user(void) {
         if (timer_elapsed32(last_sync) > 100) {
             if(transaction_rpc_send(USER_SYNC_A, sizeof(user_eyeconfig), &user_eyeconfig)) {
                 last_sync = timer_read32();
-            } 
+            }
         }
     }
 
@@ -144,6 +248,6 @@ void housekeeping_task_user(void) {
         if(raw != user_eyeconfig.raw) {
                 raw = user_eyeconfig.raw;
                 eeconfig_update_user(raw);
-        } 
+        }
     }
 }
--- keyboards/themadclover/mechboards_skull/keymaps/vial/rules.mk
+++ keyboards/themadclover/mechboards_skull/keymaps/vial/rules.mk
@@ -1,3 +1,6 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 LTO_ENABLE = yes
+
+SRC += oneshot.c
+SRC += swapper.c
