9f0fd4cd2687e13bb45f413c0303c046263cc89d
TheMadClover <130436262+TheMadClover@users.noreply.github.com>
2026-03-27 10:07:06 -0700

    add cheapino_v2

---
+++ keyboards/themadclover/cheapino_v2/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x79, 0xED, 0x40, 0xBB, 0x09, 0x8B, 0xC1, 0x9E}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
